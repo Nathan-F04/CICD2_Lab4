4f0b150fb33292dd44a496b66f186f33fb1e7413
NATHAN FERRY - STUDENT <g00413711@atu.ie>
2025-09-22 17:58:30 +0100

    created put, delete and health end points and student id constraint

--- app/schemas.py
+++ app/schemas.py
@@ -5,3 +5,4 @@ class User(BaseModel):
     name: constr(min_length=2, max_length=50)
     email: EmailStr
     age: conint(gt=18)
+    student_id: constr(min_length=7, max_length=7, pattern="S[0-9]")
