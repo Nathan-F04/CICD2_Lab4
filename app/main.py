9b3a660547682192ade6ad214f3105a1b4088b19
Nathan <G00413711@atu.ie>
2025-09-29 18:38:11 +0100

    edited test case, still unworking

--- app/main.py
+++ app/main.py
@@ -31,11 +31,11 @@ def add_user(user: User):
     users.append(user)
     return user
 
-@app.put("/api/users", status_code=200)
-def update_user(user: User):
-    for u in users:
-        if u.user_id == user.user_id:
-            users[users.index(u)] = user 
+@app.put("/api/users/{user_id}", status_code=200)
+def update_user(user_id: int, user: User):
+    for i,u in enumerate(users):
+        if u.user_id == user_id:
+            users[i] = user 
             return user
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
 
